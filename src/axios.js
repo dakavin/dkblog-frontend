ebc0de3de5865492287e8cb763ceaccb64d858de
dakavin <mikeylay@126.com>
2024-04-23 22:59:48 +0800

    解决dev环境开发下的跨域问题

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // 创建 axios 实例
 const instance = axios.create({
     // API（接口）的前缀 URL
-    baseURL:"http://localhost:8080",
+    // 在全局配置文件中，使用了正向代理，所以修改为对应的代理规则处理的前缀
+    baseURL:"/api",
     // 请求超时时间
     timeout: 7000
 })
--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,21 @@ export default defineConfig({
             // 定义一个别名 '@' 该别名对于当前 JavaScript 模块文件所在目录下的 'src' 目录的绝对文件路径
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    // 设置正向代理，使用 /api 代替后端完整的请求地址
+    // 从而解决dev环境下的跨域问题
+    server: {
+        proxy: {
+            // 对于前端的url地址中有 /api 的，就会被此代理规则处理
+            '/api': {
+                // 实际要转发的服务器（后端服务器）
+                target: 'http://localhost:8080',
+                // 在转发请求时，修改请求头中的 Origin 和 Host 字段，使其与后端服务器地址相匹配
+                changeOrigin: true,
+                // 正则：对于以 /api 为开始的url，将其替换
+                // 如：访问前端 localhost:3000/api/user 就会正向代理到后端 localhost:8080/user
+                rewrite: path => path.replace(/^\/api/,'')
+            }
+        }
     }
 })
