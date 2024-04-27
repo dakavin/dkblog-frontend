32e8e1ceee58b237c1b2bb7397a655abc5742eaa
dakavin <mikeylay@126.com>
2024-04-27 21:21:37 +0800

    实现全局引入的EP组件都是中文的

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
-
-<template>
-  <!-- html 代码 -->
-  <router-view></router-view>
-</template>
-
 <script setup>
-  /*
-  js 代码, setup 标识通常和组合式 API 搭配使用,
-  用于告诉 Vue 需要在编译时进行一些处理，让我们可以更简洁地使用组合式 API
-  */
+/*
+js 代码, setup 标识通常和组合式 API 搭配使用,
+用于告诉 Vue 需要在编译时进行一些处理，让我们可以更简洁地使用组合式 API
+*/
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
 </script>
 
-<style scoped>
-  /*
-  css 代码， scoped 表示节点内 css 样式只针对此组件有效，不影响其他组件
-  */
+<template>
+    <!-- html 代码 -->
+    <el-config-provider :locale="locale">
+        <router-view/>
+    </el-config-provider>
+</template>
 
-  /* 自定义顶部加载 Loading 颜色 */
-  #nprogress .bar {
+<style scoped>
+/*
+css 代码， scoped 表示节点内 css 样式只针对此组件有效，不影响其他组件
+*/
+
+/* 自定义顶部加载 Loading 颜色 */
+#nprogress .bar {
     background: #409eff !important;
-  }
+}
 </style>
