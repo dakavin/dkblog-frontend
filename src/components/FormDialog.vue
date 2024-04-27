dc1b490a62ab0d1e0008015bd04df9aa9a4aea18
dakavin <mikeylay@126.com>
2024-04-28 00:40:57 +0800

    修改对话框模版的bug，提交方法写错了

--- src/components/FormDialog.vue
+++ src/components/FormDialog.vue
@@ -1,21 +1,3 @@
-<template>
-    <el-dialog v-model="dialogVisible" :title="title" :width="width"
-               :destroy-on-close="destroyOnClose"
-               :draggable="true" :close-on-click-modal="false"
-               :close-on-press-escape="false">
-        <!-- 插槽 -->
-        <slot></slot>
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button @click="dialogVisible = false">取消</el-button>
-                <el-button type="primary" @click="submit">
-                    {{ confirmText }}
-                </el-button>
-            </span>
-        </template>
-    </el-dialog>
-</template>
-
 <script setup>
 import {ref} from 'vue'
 
@@ -23,12 +5,12 @@ const dialogVisible = ref(false)
 
 // 对外暴露属性
 const props = defineProps({
-    title: String,
+    title: String, // 字段类型
     width: {
         type: String,
-        default: '40%'
+        default: '40%' // 默认值
     },
-    destroyOnClose: {
+    destroyOnClose:  {
         type: Boolean,
         default: false
     },
@@ -43,8 +25,9 @@ const open = () => dialogVisible.value = true
 // 关闭
 const close = () => dialogVisible.value = false
 
+
 // 对外暴露一个 submit 方法
-const emit = defineEmits(['submit'])
+const emit  = defineEmits(['submit'])
 const submit = () => emit('submit')
 
 // 对外暴露方法
@@ -52,5 +35,25 @@ defineExpose({
     open,
     close
 })
-
 </script>
+
+<template>
+    <el-dialog v-model="dialogVisible" title="title" width="width" :draggable="true"
+               :close-on-click-modal="false" :close-on-press-escape="false"
+               align-center center>
+        <!-- 插槽 -->
+        <slot></slot>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="submit">
+                    {{ confirmText }}
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
+</template>
+
+<style scoped>
+
+</style>
