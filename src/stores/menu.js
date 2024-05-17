8aee862a4dc60135e32769867f9915b56d802a94
dakavin <mikeylay@126.com>
2024-05-17 17:02:45 +0800

    perf(pinia-menu): 优化菜单栏弹出隐藏效果

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -1,19 +1,17 @@
 import { defineStore } from "pinia";
-import { ref, computed } from 'vue';
+import { ref, watch, onUnmounted } from 'vue';
 
 export const useMenuStore = defineStore('menu', () => {
     const menuWidth = ref("250px");
     const isMobile = ref(window.innerWidth <= 768);
     const menuVisible = ref(false); // 默认在移动端隐藏
 
-
     // 控制菜单宽度和可见性的函数
     function handleMenuWidth() {
         if (isMobile.value) {
-            menuVisible.value = !menuVisible.value; // 切换菜单的可见性
-            menuWidth.value = menuVisible.value ? '250px' : '0px'; // 根据可见性调整宽度
+            menuVisible.value = !menuVisible.value;
+            menuWidth.value = menuVisible.value ? '250px' : '0px';
         } else {
-            // PC端时，宽度在64px和250px之间切换，菜单始终可见
             menuWidth.value = menuWidth.value === '250px' ? '64px' : '250px';
         }
     }
@@ -21,19 +19,33 @@ export const useMenuStore = defineStore('menu', () => {
     // 响应窗口大小变化，更新设备类型和菜单状态
     function updateMenuWidthForDevice() {
         isMobile.value = window.innerWidth <= 768;
+        menuWidth.value = isMobile.value ? '0px' : (menuVisible.value ? '250px' : '64px');
         if (isMobile.value) {
-            // 手机端默认不显示菜单
             menuVisible.value = false;
-            menuWidth.value = '0px';
-        } else {
-            // PC端根据当前宽度设置
-            menuWidth.value = menuVisible.value ? '250px' : '64px';
         }
     }
 
-    window.addEventListener('resize', updateMenuWidthForDevice);
+    // 监听窗口大小变化并使用防抖处理
+    let resizeTimer;
+    function handleResize() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(updateMenuWidthForDevice, 150);
+    }
+    window.addEventListener('resize', handleResize);
 
-    return { menuWidth, handleMenuWidth, menuVisible,isMobile};
+    // 组件卸载时移除事件监听
+    onUnmounted(() => {
+        window.removeEventListener('resize', handleResize);
+    });
+
+    // 监听isMobile变化
+    watch(isMobile, (newValue) => {
+        if (!newValue) {
+            menuVisible.value = true; // PC端默认显示菜单
+        }
+    });
+
+    return { menuWidth, handleMenuWidth, menuVisible, isMobile };
 }, {
     persist: true
 });
