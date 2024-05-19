9312ebe8a9b671a29d46cb583003a79a3c85a62b
dakavin <mikeylay@126.com>
2024-05-19 21:14:56 +0800

    test(all): 测试上传到两个仓库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 /*
 js 代码, setup 标识通常和组合式 API 搭配使用,
-用于告诉 Vue 需要在编译时进行一些处理，让我们可以更简洁地使用组合式 API
+用于告诉Vue需要在编译时进行一些处理，让我们可以更简洁地使用组合式 API
 */
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // import cursorInit from "@/composables/cursor.js";
